64950f081997b121beabbcae48f795517c3871f6
georgeabedgit config --global user.name georgeabedgit config --global user.name georgeabed <georges.abed@hotmail.com>
2020-02-12 15:10:22 +0100

    TP Edition and Export (12/02/2020)

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,11 +6,20 @@ import com.example.demo.entity.Facture;
 import com.example.demo.service.impl.ClientServiceImpl;
 import com.example.demo.service.ArticleService;
 import com.example.demo.service.FactureService;
+
+import org.apache.poi.ss.usermodel.DataConsolidateFunction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import java.time.LocalDate;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Controller principale pour affichage des clients / factures sur la page d'acceuil.
@@ -18,29 +27,110 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    private ArticleService articleService;
-    private ClientServiceImpl clientServiceImpl;
-    private FactureService factureService;
+	private ArticleService articleService;
+	private ClientServiceImpl clientServiceImpl;
+	private FactureService factureService;
 
-    public HomeController(ArticleService articleService, ClientServiceImpl clientService, FactureService factureService) {
-        this.articleService = articleService;
-        this.clientServiceImpl = clientService;
-        this.factureService = factureService;
-    }
+	public HomeController(ArticleService articleService, ClientServiceImpl clientService, FactureService factureService) {
+		this.articleService = articleService;
+		this.clientServiceImpl = clientService;
+		this.factureService = factureService;
+	}
 
-    @GetMapping("/")
-    public ModelAndView home() {
-        ModelAndView modelAndView = new ModelAndView("home");
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("home");
 
-        List<Article> articles = articleService.findAll();
-        modelAndView.addObject("articles", articles);
+		List<Article> articles = articleService.findAll();
+		modelAndView.addObject("articles", articles);
 
-        List<Client> toto = clientServiceImpl.findAllClients();
-        modelAndView.addObject("clients", toto);
+		List<Client> toto = clientServiceImpl.findAllClients();
+		modelAndView.addObject("clients", toto);
 
-        List<Facture> factures = factureService.findAllFactures();
-        modelAndView.addObject("factures", factures);
+		List<Facture> factures = factureService.findAllFactures();
+		modelAndView.addObject("factures", factures);
 
-        return modelAndView;
-    }
+		return modelAndView;
+	}
+
+	/*
+    @GetMapping("/articles/csv")
+    public void articlesCSV(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    	response.setContentType("text/csv");
+    	response.setHeader("Content-Disposition", "attachment; filename=\"export-articles.csv\"");
+    			PrintWriter writer = response.getWriter();
+
+    			writer.println("LIGNE1COLONNE1;LIGNE1COLONNE2");
+    			writer.println("LIGNE2COLONNE1;LIGNE2COLONNE2");
+    		}
+	 */
+
+	@GetMapping("/articles/csv")
+	public void articlesCSV(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition", "attachment; filename=\"export-articles.csv\"");
+		PrintWriter writer = response.getWriter();
+
+		List<Article> articles = articleService.findAll();
+
+		for(int i=0;i<articles.size();i++) {
+
+			Article article = articles.get(i);
+			writer.println(article.getLibelle() + ";" + article.getPrix());
+
+		}
+
+
+	}
+
+	@GetMapping("/clients/csv")
+	public void clientesCSV(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition", "attachment; filename=\"export-clients.csv\"");
+		PrintWriter writer = response.getWriter();
+
+		List<Client> clients = clientServiceImpl.findAllClients();
+
+		for(int i=0;i<clients.size();i++) {
+
+			Client c = clients.get(i);
+
+			LocalDate DateNow = LocalDate.now(); 
+			Integer anneeDateNaissance = c.getDateNaissance().getYear();
+			Integer anneeNow = DateNow.getYear(); 	
+
+			Integer age = anneeNow - anneeDateNaissance;
+
+			writer.println(c.getNom() + ";" + c.getPrenom() + ";" + age);
+
+		}
+	}
+
+	@GetMapping("/clients/xlsx")
+	public void clientsXLSX(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+		response.setHeader("Content-Disposition", "attachment; filename=\"export-clients.xlsx\"");
+		PrintWriter writer = response.getWriter();
+
+		List<Client> clients = clientServiceImpl.findAllClients();
+
+		for(int i=0;i<clients.size();i++) {
+
+			Client c = clients.get(i);
+
+			LocalDate DateNow = LocalDate.now(); 
+			Integer anneeDateNaissance = c.getDateNaissance().getYear();
+			Integer anneeNow = DateNow.getYear(); 	
+
+			Integer age = anneeNow - anneeDateNaissance;
+
+			writer.println(c.getNom() + c.getPrenom() + age);
+
+		}
+	}
+
+
 }
+
+
+
